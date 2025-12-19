39108826a44e305a8ab1eca1cee531158e2bb480
witsao-oya <witsaoo@gmail.com>
2025-12-19 19:23:01 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'fenta-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './fenta_logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
